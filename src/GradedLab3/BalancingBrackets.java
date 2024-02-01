3ccfbccce7e171ded7c34b60676845df4fbbeb0c
sakshimangla11 <133721159+sakshimangla11@users.noreply.github.com>
2024-02-01 07:36:25 +0530

    Add files via upload

---
+++ src/GradedLab3/BalancingBrackets.java
@@ -0,0 +1,59 @@
+package GradedLab3;
+
+import java.util.Stack;
+
+public class BalancingBrackets {
+
+	static boolean checkingBracketsBalanced(String bracketExpression) {
+        Stack<Character> stack = new Stack<Character>();
+
+        for(int i=0;i<bracketExpression.length();i++) {
+        	char character = bracketExpression.charAt(i);
+        	if(character == '(' || character == '[' || character=='{') {
+        		stack.push(character);
+        		continue;
+        	}
+        
+        
+        if(stack.isEmpty()) {
+        	return false;
+        }
+        
+        char c;
+        switch(character) {
+            case '}':
+            	c = stack.pop();
+            	if(c == '(' || c == '[') 
+            		return false;
+            	break;
+            case  ')' :
+            	c =  stack.pop();
+            	if(c =='{' || c== '[') {
+            		return false;
+            	}
+            	break;
+            case ']' :
+            	c =  stack.pop();
+            	if(c == '(' || c == '{') {
+            		return false;
+            	}
+                break;      
+        }
+	  }
+        return(stack.isEmpty());
+	}
+	
+	public static void main(String[] args) {
+		String bracketExpression = "([[{}]])";
+		
+		Boolean result;
+		
+		result = checkingBracketsBalanced(bracketExpression);
+		if(result) {
+			System.out.println("The entered String has balanced brackets");		
+		}
+		else
+			System.out.println("The entered Strings do not contain Balanced brackets");		
+
+	}
+}
